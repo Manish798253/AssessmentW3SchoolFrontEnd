7152ca6ebd426bc159c8d6bed75252115b39b6e1
manish moryani <manish.moryani.1@gmail.com>
2022-10-02 00:23:27 +0530

    index,css,js file added

---
+++ script.js
@@ -0,0 +1,18 @@
+var pullDownHandleBars=document.getElementsByClassName("black");
+//console.log(pullDownHandleBars.item(0));
+//var element=pullDownHandleBars[0];
+var l=pullDownHandleBars.length;
+var element=undefined;
+for(let i=0;i<l;i++)
+{
+element=pullDownHandleBars[i];
+console.log(element);
+element.addEventListener("click",f);
+console.log(i);
+}
+function f()
+{
+    console.log(this);
+    //open the html related content
+}
+

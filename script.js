35a223ad5ef02b956d1013eb3886c9620687751b
manish moryani <manish.moryani.1@gmail.com>
2022-10-02 01:40:58 +0530

    adding html onclick

--- script.js
+++ script.js
@@ -8,12 +8,29 @@ for(let i=0;i<l;i++)
 element=pullDownHandleBars[i];
 console.log(element);
 element.addEventListener("click",f);
+element.visibility=false;
 console.log(i);
 }
+var div_element=undefined;
 function f()
 {
     console.log(this);
+    this.visibility=!this.visibility;
+    if(this.visibility==true)
+    {
     //open the html related content
+   div_element=document.createElement("div");
+    div_element.style.width="500px";
+    div_element.style.height="200px";
+    div_element.style.backgroundColor="black";
+   this.appendChild(div_element);
+    }
+    else
+    {
+        console.log(this);
+      this.firstChild.innerHTML="";
+    }
+ 
 }
 console.log("yes")
 
